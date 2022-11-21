f13b6cc5826c24bf0f4b2041d7ed4a415c44dc57
Deekshith Byloli <58661817+deekshithbyloli@users.noreply.github.com>
2022-11-21 13:18:05 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class reArrange {
+	
+	static void rearrange(int[] A, int N)
+    {
+        int temp[] = A.clone();
+        int low = 0, high = N - 1;
+        boolean flag = true;
+ 
+        // Store result in temp[]
+        for (int i = 0; i < N; i++) {
+            if (flag)
+            	 A[i] = temp[low++];
+            else
+            	A[i] = temp[high--];
+ 
+            flag = !flag;
+        }
+    }
+	
+	public static void printArray(int[] A) {
+        for (int j : A) {
+            System.out.print(j + " ");
+        }
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+        int[] A = new int[size];
+        for (int i = 0; i < size; i++) {
+            A[i] = scan.nextInt();
+        }
+        rearrange(A, size);
+        printArray(A);
+
+	}
+
+}
