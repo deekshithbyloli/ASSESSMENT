9b148cba2c52d317594578736a68db5e47886b39
Deekshith Byloli <58661817+deekshithbyloli@users.noreply.github.com>
2022-11-21 13:22:28 +0530

    Create coffe.js

---
+++ coffe.js
@@ -0,0 +1,25 @@
+//for displaying menu 
+let coffeeMenu =  {
+      espresso: {
+        milk: 60, 
+        cream: 75, 
+        latte: 100
+        },
+      cappuccino: {
+        milk: 80, 
+        cream: 90, 
+        latte: 125 
+        },
+      latte: {
+        milk: 100, 
+        cream: 125, 
+        latte: 150
+        }
+    }
+
+
+//setting up input for customers
+
+coffeeMenu.espresso.milk;  // 60
+coffeeMenu.cappuccino.latte;  // 125
+coffeeMenu.latte.cream;  // 125
